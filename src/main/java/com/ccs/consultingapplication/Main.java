4d3b3fba481d2602eb2468ee9f06042122a670e9
Nasiruddin Mohammed <nasirlambton@gmail.com>
2021-03-11 02:17:30 -0500

    hello

---
+++ src/main/java/com/ccs/consultingapplication/Main.java
@@ -0,0 +1,10 @@
+package com.ccs.consultingapplication;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("H");
+        Person person = new Person("Mohammed");
+        System.out.println(person.getName());
+
+    }
+    }
